197dcab93dd0e612e458b66294ba520ec7b5eceb
Kevin Allen <kevin.allen91@gmail.com>
2020-08-01 19:50:09 -0700

    created ball and paddle objects

--- pong.js
+++ pong.js
@@ -16,3 +16,33 @@ function drawRect(x, y, w, h, color) {
     ctx.fillStyle = color
     ctx.fillRect(x,y,w,h) 
 }
+
+const ball = {
+    x: canvas.width/2,
+    y: canvas.height/2,
+    radius: 10,
+    velocityX: 5,
+    velocityY: 5,
+    speed: 7,
+    color: 'WHITE'
+}
+
+// User Paddle
+const user = {
+    x: 0, // left side of canvas
+    y: (canvas.height - 100)/2, // sets paddle below canvas
+    width: 10,
+    height: 100,
+    score: 0,
+    color: 'WHITE'
+}
+
+// Computer Paddle
+const comp = {
+    x: canvas.width - 10,
+    y: (canvas.height - 100)/2,
+    width: 10,
+    height: 100,
+    score: 0,
+    color: 'WHITE'
+}
