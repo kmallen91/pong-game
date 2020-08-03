fff1c649a7ea770a1bcef3c08ad1af75361343aa
Kevin Allen <kevin.allen91@gmail.com>
2020-08-03 14:28:59 -0700

    WIP arrow key listener

--- pong.js
+++ pong.js
@@ -77,10 +77,10 @@ function checkKey(e) {
     e = e || window.event;
 
     if (e.keyCode == '38') {
-        // up arrow
+    // up arrow
     }
     else if (e.keyCode == '40') {
-        // down arrow
+    // down arrow
     }
     else if (e.keyCode == '37') {
     // left arrow
